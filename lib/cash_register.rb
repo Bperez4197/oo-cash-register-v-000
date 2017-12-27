b99760998fdc7005243fe29604c1cc0ebb30cf4a
Bperez4197 <bryceperez31@gmail.com>
2017-12-27 08:45:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,12 @@ class CashRegister
 
   attr_accessor :tota, :discount
 
+  @@items = []
+  @@last_item = []
     def initialize(discount=0)
       @total = 0
       @discount = discount
+      @@items.clear
     end
 
     def total
